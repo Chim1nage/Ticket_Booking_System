bfa7c24e9a75fac311272a72b646b2e5fd83305e
Chim1nage <98669225+Chim1nage@users.noreply.github.com>
2024-04-15 15:39:42 -0400

    Solved syntax bug

--- Database/TicketDB.sql
+++ Database/TicketDB.sql
@@ -129,7 +129,7 @@ DELIMITER ;
         
 -- PROCEDURES
 DELIMITER
-//
+$$
 CREATE PROCEDURE get_all_ticket_information_related_to_user(IN user_id VARCHAR (255))
 BEGIN
 select t.ticket_id,
@@ -149,12 +149,11 @@ from ticket as t
          join stadium as st on s.stadium_id = st.stadium_id
          join event as e on e.event_id = t.event_id
 where t.buyer = user_id;
-END
-//
+END$$
 DELIMITER ;
 
 DELIMITER
-//
+$$
 CREATE PROCEDURE get_ticket_from_seat_stadium_event(IN seat_id_ INT, IN stadium_id_ INT, IN event_id_ INT)
 BEGIN
 SELECT ticket_id
@@ -162,12 +161,11 @@ FROM ticket
 WHERE seat_id = seat_id_
   AND stadium_id = stadium_id_
   AND event_id = event_id_;
-END
-//
+END$$
 DELIMITER ;
 
 DELIMITER
-//
+$$
 CREATE PROCEDURE update_ticket(IN booking_date_ DATE, IN first_name_ VARCHAR (255), IN last_name_ VARCHAR (255),
                                IN buyer_ VARCHAR (255), IN ticket_id_ INT)
 BEGIN
@@ -177,12 +175,11 @@ SET booking_date = booking_date_,
     last_name    = last_name_,
     buyer        = buyer_
 WHERE ticket_id = ticket_id_;
-END
-//
+END$$
 DELIMITER ;
 
 DELIMITER
-//
+$$
 CREATE PROCEDURE reset_ticket_trading_information(IN ticket INT)
 BEGIN
 UPDATE TICKET
@@ -192,44 +189,40 @@ SET booking_date   = null,
     buyer_account  = null,
     seller_account = null
 WHERE ticket_id = ticket;
-END
-//
+END$$
 DELIMITER ;
 
 DELIMITER
-//
+$$
 CREATE PROCEDURE create_event(IN name_ VARCHAR (255), IN date_ DATE, IN event_ ENUM("Sport", "Concert", "Art & Theater",
                               "Family", "Other"), IN stadium_ INT, IN host_ VARCHAR (64))
 BEGIN
 INSERT INTO event(event_name, event_date, event_type, stadium_id, host_username)
 VALUES (name_, date_, event_, stadium_, host_);
-END
-//
+END$$
 DELIMITER ;
 
 DELIMITER
-//
+$$
 CREATE PROCEDURE get_all_event()
 BEGIN
 select *
 from event;
-END
-//
+END$$
 DELIMITER ;
 
 DELIMITER
-//
+$$
 CREATE PROCEDURE get_all_event_with_stadium_name()
 BEGIN
 SELECT e.*, s.stadium_name
 FROM event AS e
          JOIN stadium AS s ON s.stadium_id = e.stadium_id;
-END
-//
+END$$
 DELIMITER ;
 
 DELIMITER
-//
+$$
 CREATE PROCEDURE get_event_id(IN name_ VARCHAR (255), IN date_ DATE, IN event_ ENUM("Sport", "Concert", "Art & Theater",
                               "Family", "Other"), IN stadium_ INT, IN host_ VARCHAR (64))
 BEGIN
@@ -240,44 +233,40 @@ WHERE event_name = name_
   AND event_type = event_
   AND stadium_id = stadium_
   AND host_username = host_;
-END
-//
+END$$
 DELIMITER ;
 
 DELIMITER
-//
+$$
 CREATE PROCEDURE delete_event(IN event_id_ INT)
 BEGIN
 DELETE
 FROM event
 WHERE event_id = event_id_;
-END
-//
+END$$
 DELIMITER ;
 
 DELIMITER
-//
+$$
 CREATE PROCEDURE get_all_stadium()
 BEGIN
 select *
 from stadium;
-END
-//
+END$$
 DELIMITER ;
 
 DELIMITER
-//
+$$
 CREATE PROCEDURE get_all_seat_id_from_stadium(IN stadium_ INT)
 BEGIN
 SELECT seat_id
 FROM seat
 WHERE stadium_id = stadium_;
-END
-//
+END$$
 DELIMITER ;
 
 DELIMITER
-//
+$$
 CREATE PROCEDURE get_all_empty_seat_from_stadium_for_event(IN event_id_ INT)
 BEGIN
 SELECT s.*
@@ -287,12 +276,11 @@ FROM seat s
          JOIN ticket AS t ON t.event_id = e.event_id
 WHERE e.event_id = event_id_
   AND t.buyer = null;
-END
-//
+END$$
 DELIMITER ;
 
 DELIMITER
-//
+$$
 CREATE PROCEDURE get_seat_from_row_number_stadium(IN row_ INT, IN num_ INT, IN stadium_id_ INT)
 BEGIN
 SELECT s.seat_id
@@ -300,69 +288,62 @@ FROM seat AS s
          JOIN stadium AS st on s.stadium_id = st.stadium_id
 WHERE s.seat_row = row_
   AND s.seat_number = num_;
-END
-//
+END$$
 DELIMITER ;
 
 DELIMITER
-//
+$$
 CREATE PROCEDURE create_ticket(IN price_ INT, IN seat_id_ INT, IN stadium_id_ INT, IN event_id_ INT)
 BEGIN
 INSERT INTO ticket(price, seat_id, stadium_id, event_id)
 VALUES (price_, seat_id_, stadium_id_, event_id_);
-END
-//
+END$$
 DELIMITER ;
 
 DELIMITER
-//
+$$
 CREATE PROCEDURE get_ticket(IN ticket_id_ INT)
 BEGIN
 SELECT *
 FROM ticket
 WHERE ticket_id = ticket_id_;
-END
-//
+END$$
 DELIMITER ;
 
 DELIMITER
-//
+$$
 CREATE PROCEDURE update_ticket_name(IN first VARCHAR (255), IN last VARCHAR (255), IN ticket INT)
 BEGIN
 UPDATE ticket
 SET first_name = first, last_name = last
 WHERE ticket_id = ticket;
-END
-//
+END$$
 DELIMITER ;
 
 DELIMITER
-//
+$$
 CREATE PROCEDURE delete_all_ticket_related_to_event(IN event_id_ INT)
 BEGIN
 DELETE
 FROM ticket
 WHERE event_id = event_id_;
-END
-//
+END$$
 DELIMITER ;
 
 DELIMITER
-//
+$$
 CREATE PROCEDURE get_field_from_table(IN field_name VARCHAR (255), IN table_name VARCHAR (255))
 BEGIN
 SELECT field_name
 FROM table_name;
-END
-//
+END$$
 DELIMITER ;
 
 DELIMITER
-//
+$$
 CREATE PROCEDURE get_all_host()
 BEGIN
 SELECT *
 FROM host;
-END
-//
+END$$
 DELIMITER ;
